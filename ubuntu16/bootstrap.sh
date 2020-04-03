28121978dfa1017fa5ec6c8bbf0a1b1027386af0
sfinlon <finlon@gmail.com>
2020-04-03 11:36:07 -0400

    move ubuntu16 to python3

--- ubuntu16/bootstrap.sh
+++ ubuntu16/bootstrap.sh
@@ -9,10 +9,15 @@ export CIF_HUNTER_ADVANCED=$CIF_HUNTER_ADVANCED
 export CIF_GATHERER_GEO_FQDN=$CIF_GATHERER_GEO_FQDN
 
 echo 'installing the basics'
-sudo apt-get update && apt-get install -y build-essential python-dev python2.7 python-pip python-dev aptitude \
-    python-pip libffi-dev libssl-dev sqlite3 software-properties-common
+apt-get update && apt-get upgrade -y
+apt-get install -y gcc git python3-minimal python3-pip unzip
 
-sudo pip install pip --upgrade
+update-alternatives --install /usr/bin/python python /usr/bin/python3.5 1
+update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+
+pip install --upgrade pip cryptography
+apt-get --auto-remove --yes remove python3-yaml
+
 
 echo 'checking for python-openssl'
 set +e
@@ -23,7 +28,7 @@ if [[ ! -z ${EXISTS} ]]; then
 	echo "#@link https://github.com/csirtgadgets/bearded-avenger-deploymentkit/issues/15"
 	echo "# sudo apt-get --auto-remove --yes remove python-openssl"
 	echo "# sudo pip install pyOpenSSL"
-	sudo apt-get --auto-remove --yes remove python-openssl && sudo pip install pyOpenSSL
+	sudo apt-get --auto-remove --yes remove python-openssl && sudo pip3 install pyOpenSSL
 fi
 
 sudo pip install 'pytest>=2.8.0,<3.0'
